cf11c00d7bf89f50ac54c321749ccea7d15ab75d
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-03 11:45:22 +0100

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -8,7 +8,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule,MatSidenavModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule,MatSidenavModule, RouterLinkActive, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
