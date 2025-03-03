205c81ce2713dedbea357c28dd2362c4779fcaeb
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-03 13:11:10 +0100

    added ad user dialog

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    DialogAddUserComponent,
+  ],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddUserComponent, {
+      height: '400px',
+      width: '600px',
+      // data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      //   this.animal = result;
+    });
+  }
 }
