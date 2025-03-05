08be7497abf27cb5261cf342f9d4d3730a247017
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-05 11:16:26 +0100

    hide firestore API Key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,22 +5,14 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../enviroment';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     provideAnimationsAsync(),
     // importProvidersFrom(provideFirestore(() => getFirestore())),
     // importProvidersFrom(
-      provideFirebaseApp(() =>
-        initializeApp({
-          projectId: 'simple-crm-33f69',
-          appId: '1:24672667530:web:d631a13fd4a22c89002a73',
-          storageBucket: 'simple-crm-33f69.firebasestorage.app',
-          apiKey: 'AIzaSyAmoAOyG-GNX1i3x8CYf5Oh1sECrG4bvYI',
-          authDomain: 'simple-crm-33f69.firebaseapp.com',
-          messagingSenderId: '24672667530',
-        })
-      ),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
     // ),
     provideFirestore(() => getFirestore()),
   ],
