4a773c565c463df2cd38a8fab15b7b8e0b3ac305
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-09 20:46:55 +0100

    on snapshot

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -3,7 +3,7 @@ import {MatCardModule} from '@angular/material/card';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
 import { User } from '../../models/user.class';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { inject } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import {MatIconModule} from '@angular/material/icon';
@@ -25,19 +25,32 @@ export class UserDetailComponent {
   singleUserId = '';
   firestore = inject(Firestore);
 
-  constructor(private route:ActivatedRoute, private firebaseService: FirebaseService, public dialog: MatDialog){}
+  unsubUser;
+
+  constructor(private route:ActivatedRoute, private firebaseService: FirebaseService, public dialog: MatDialog){
+    this.unsubUser = this.subSingleUser();
+  }
 
   async ngOnInit() {
-    this.getId();
-    this.getSingleUser(this.singleUserId)
+    // this.getId();
+    // this.getSingleUser(this.singleUserId)
   }
 
-  async getSingleUser(id:string) {
-    const docRef = doc(this.firestore, 'user', id);
-    this.user = await getDoc(docRef);
-    this.user = this.user.data();
-    this.user.id = this.singleUserId;
+  async subSingleUser() {
+    this.getId()
+    const docRef = doc(this.firestore, 'user', this.singleUserId);
+    return onSnapshot(doc(this.firestore, "user", this.singleUserId), (doc) => {
+      // this.user = getDoc(docRef);
+      this.user = doc.data();
+      this.user.id = this.singleUserId;
+    });
   }
+  // async getSingleUser(id:string) {
+  //   const docRef = doc(this.firestore, 'user', id);
+  //   this.user = await getDoc(docRef);
+  //   this.user = this.user.data();
+  //   this.user.id = this.singleUserId;
+  // }
 
   getId() {
     this.singleUserId = this.route.snapshot.paramMap.get('id') || '';
