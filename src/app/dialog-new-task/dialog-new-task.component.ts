01115699300df7438edb4b21d8f74566c5d7bae5
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-10 23:03:26 +0100

    open new task dialog

---
+++ src/app/dialog-new-task/dialog-new-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-new-task.component.html',
+  styleUrl: './dialog-new-task.component.scss'
+})
+export class DialogNewTaskComponent {
+
+}
--- src/app/simple-opl/simple-opl.component.ts
+++ src/app/simple-opl/simple-opl.component.ts
@@ -4,12 +4,17 @@ import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MatIcon } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DialogNewTaskComponent } from '../dialog-new-task/dialog-new-task.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { FirebaseService } from '../services/firebase.service';
 
 
 @Component({
   selector: 'app-simple-opl',
   standalone: true,
-  imports: [MatCardModule,MatCheckboxModule, FormsModule, CommonModule, MatIcon],
+  imports: [MatCardModule,MatCheckboxModule, FormsModule, CommonModule, MatIcon, MatTooltipModule, MatDialogModule],
   templateUrl: './simple-opl.component.html',
   styleUrl: './simple-opl.component.scss'
 })
@@ -17,8 +22,13 @@ export class SimpleOPLComponent {
 
   checked = false;
 
+  constructor(
+      public dialog: MatDialog,
+      private firebaseService: FirebaseService
+    ) {}
+
   openDialog() {
-    console.log('hier entsteht eine neue Dialogbox');
+    const dialogRef = this.dialog.open(DialogNewTaskComponent, {});
 
   }
 
