5c02d68a3198f207858797ce1dbf82316e6b157a
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-13 12:22:28 +0100

    implemented back button detail customer

--- src/app/customer-detail/customer-detail.component.ts
+++ src/app/customer-detail/customer-detail.component.ts
@@ -1,38 +1,48 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 import { inject } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { EditAddressDialogComponent } from '../edit-address-dialog/edit-address-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { EditHeadDialogComponent } from '../edit-head-dialog/edit-head-dialog.component';
 import { FormsModule } from '@angular/forms';
 import { DialogChangeCustomerStatusComponent } from '../dialog-change-customer-status/dialog-change-customer-status.component';
 import { CommonModule } from '@angular/common';
 import { DialogDeleteCustomerComponent } from '../dialog-delete-customer/dialog-delete-customer.component';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-detail',
   standalone: true,
-  imports: [MatCardModule, MatIconModule,MatMenuModule, FormsModule, CommonModule],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './customer-detail.component.html',
-  styleUrl: './customer-detail.component.scss'
+  styleUrl: './customer-detail.component.scss',
 })
 export class CustomerDetailComponent {
-
   customer: any = {};
   singleCustomerId = '';
   firestore = inject(Firestore);
-  customerType:string = ' ';
+  customerType: string = ' ';
 
   unsubCustomer;
 
-  constructor(private route:ActivatedRoute, private firebaseService: FirebaseService, public dialog: MatDialog){
+  constructor(
+    private route: ActivatedRoute,
+    private firebaseService: FirebaseService,
+    public dialog: MatDialog,
+    private router: Router
+  ) {
     this.unsubCustomer = this.subSingleCustomer();
   }
 
@@ -41,48 +51,47 @@ export class CustomerDetailComponent {
   //   // this.getSingleUser(this.singleUserId)
   // }
 
-
-
   async subSingleCustomer() {
-    this.getId()
+    this.getId();
     const docRef = doc(this.firestore, 'customer', this.singleCustomerId);
-    return onSnapshot(doc(this.firestore, "customer", this.singleCustomerId), (doc) => {
-      this.customer = doc.data();
-      this.customer.id = this.singleCustomerId;
-      if (this.customer.new === true) {
-        this.customerType = 'Neukunde'
-      } else if (this.customer.existing === true) {
-        this.customerType = "Bestandskunde"
-      } else if (this.customer.vip === true) {
-        this.customerType = "V.I.P."
+    return onSnapshot(
+      doc(this.firestore, 'customer', this.singleCustomerId),
+      (doc) => {
+        this.customer = doc.data();
+        this.customer.id = this.singleCustomerId;
+        if (this.customer.new === true) {
+          this.customerType = 'Neukunde';
+        } else if (this.customer.existing === true) {
+          this.customerType = 'Bestandskunde';
+        } else if (this.customer.vip === true) {
+          this.customerType = 'V.I.P.';
+        }
       }
-    });
+    );
   }
 
   getId() {
     this.singleCustomerId = this.route.snapshot.paramMap.get('id') || '';
   }
 
-  openDeleteCustomerDialog(customer:any) {
-    const dialogRef = this.dialog.open(DialogDeleteCustomerComponent, {
-    });
+  openDeleteCustomerDialog(customer: any) {
+    const dialogRef = this.dialog.open(DialogDeleteCustomerComponent, {});
     dialogRef.componentInstance.customer = customer;
   }
 
-  openEditAddressDialog(singleCustomer:any):void {
-    const dialogRef = this.dialog.open(EditAddressDialogComponent, {
-        });
-        dialogRef.componentInstance.customer = singleCustomer;
+  openEditAddressDialog(singleCustomer: any): void {
+    const dialogRef = this.dialog.open(EditAddressDialogComponent, {});
+    dialogRef.componentInstance.customer = singleCustomer;
     console.log(singleCustomer);
   }
 
-  openEditHeaderDialog(singleCustomer:any):void {
+  openEditHeaderDialog(singleCustomer: any): void {
     const dialogRef = this.dialog.open(EditHeadDialogComponent);
     dialogRef.componentInstance.customer = singleCustomer;
     console.log(singleCustomer);
   }
 
-  openChangeCustomerStatusDialog(singleCustomer:any) {
+  openChangeCustomerStatusDialog(singleCustomer: any) {
     const dialogRef = this.dialog.open(DialogChangeCustomerStatusComponent);
     dialogRef.componentInstance.customer = singleCustomer;
     console.log(singleCustomer);
@@ -96,4 +105,13 @@ export class CustomerDetailComponent {
     };
   }
 
+  backToCustomerOverview(customerType:string) {
+    if (customerType === 'Neukunde') {
+      this.router.navigate(['/newCustomersOverview']);
+    } else if (customerType === 'Bestandskunde') {
+      this.router.navigate(['/existingCustomersOverview']);
+    } else if (customerType === 'V.I.P.') {
+      this.router.navigate(['/vipCustomersOverview']);
+    }
+  }
 }
